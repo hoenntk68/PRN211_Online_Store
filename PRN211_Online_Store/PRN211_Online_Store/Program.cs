0f7c5fa50474e004bd6fbb347e21734f77a57bdc
hoenntk68 <hn8319542@gmail.com>
2023-07-04 00:37:15 +0700

    add mockup and init project on visual studio

---
+++ PRN211_Online_Store/PRN211_Online_Store/Program.cs
@@ -0,0 +1,15 @@
+namespace PRN211_Online_Store
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
